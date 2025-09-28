6da738724c0e4be070f952ea0f884555a402f193
Steven88ly <steven88ly@gmail.com>
2025-09-27 22:57:50 -0400

    add init boiler plate stuff for backend
    added traintype and city enums

---
+++ backend/src/enums/train-types-enums.ts
@@ -0,0 +1,19 @@
+export const enum TrainType {
+  AVE = "AVE",
+  EUROCITY = "EuroCity",
+  EUROSTAR = "Eurostar",
+  FRECCIAROSSA = "Frecciarossa",
+  IC = "IC",
+  ICE = "ICE",
+  INTERCITY = "InterCity",
+  INTERCITES = "Intercités",
+  ITALO = "Italo",
+  NIGHTJET = "Nightjet",
+  RE = "RE",
+  RJX = "RJX",
+  RAILJET = "Railjet",
+  REGIOEXPRESS = "RegioExpress",
+  TER = "TER",
+  TGV = "TGV",
+  THALYS = "Thalys"
+}
