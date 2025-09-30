b060dc4c0a38cdcfe64f79c9faf8b544a5c1e30b
Steven88ly <steven88ly@gmail.com>
2025-09-30 15:49:34 -0400

    developed dfs algo to grab all connections <=3 connections

--- backend/backend_django/services/classes/station.py
+++ backend/backend_django/services/classes/station.py
@@ -4,13 +4,18 @@ from ..classes.Connection import Connection
 class Station:
     def __init__(self, city ):
         self.city = city
-        self.outgoing_connections = {}  # key: DayOfWeek, value: list of connections
+        self.outgoing_connections = {}  # key: DayOfWeek, value: map of arrival city to list of connections
         
     def add_connection(self, connection: Connection):
         
         for day_of_week in connection.days_of_operation:
         
             if day_of_week not in self.outgoing_connections:
-                self.outgoing_connections[day_of_week] = []
-            self.outgoing_connections[day_of_week].append(connection)
+                self.outgoing_connections[day_of_week] = {}
+            
+            if connection.arrival_city not in self.outgoing_connections[day_of_week]:
+                self.outgoing_connections[day_of_week][connection.arrival_city] = []
+                
+            self.outgoing_connections[day_of_week][connection.arrival_city].append(connection)
+            
         
--- backend/backend_django/services/classes/station_network_manager.py
+++ backend/backend_django/services/classes/station_network_manager.py
@@ -1,6 +1,9 @@
 # graph class
 from ..csv_read import read_csv
 from .station import Station
+from ..enums.city_enum import City
+from ..enums.day_of_week_enum import DayOfWeek
+from .trip import Trip
 
 class StationNetworkManager:
     def __init__(self):
@@ -30,6 +33,52 @@ class StationNetworkManager:
             
             station = self.stations[connection.departure_city]
             station.add_connection(connection)
+            
+        list_of_trips = list(self.dfs_all_paths(City.AMSTERDAM, City.EINDHOVEN, DayOfWeek.Saturday))
+        
+        if(len(list_of_trips) == 0):
+            print("No trips found.")
+            
+        for trip in list_of_trips:
+            print(f"Trip from {trip.DepartureCity.value} to {trip.ArrivalCity.value} with {len(trip.connections)} connections.\n Total duration: {trip.TotalTravelDuration}, Total first class price: {trip.TotalFirstClassPrice} euro.")
+       
+
+            
+    def dfs_all_paths(self, start_city : City, end_city :City, day_of_week : DayOfWeek):
+        
+        all_paths = [] # list of lists of connections
+        
+        # direct connection check
+        # if(self.stations.get(start_city).outgoing_connections.get(day_of_week).get(end_city) is not None):
+        #     for connection in self.stations.get(start_city).outgoing_connections.get(day_of_week).get(end_city):
+        #         all_paths.append(Trip([connection]))
+            
+          
+        stack = [(start_city, [])]  # (current_city, path_so_far, visited_cities)
+        
+        while stack:
+            
+            current_city, path_so_far = stack.pop()
+            
+            if current_city == end_city and len(path_so_far) <= 3:
+                all_paths.append(Trip(path_so_far))
+                continue
+            
+            # limit to max 2 connections (3 legs)
+            if len(path_so_far) >= 2 or self.stations.get(current_city).outgoing_connections.get(day_of_week) is None:
+                continue
+            
+            # explore all neighbouring connections
+            for next_city, connections in self.stations.get(current_city).outgoing_connections.get(day_of_week).items():
+                for connection in connections:
+                    if connection not in path_so_far:  # avoid cycles
+                        # ensure chronological order, passenger can make it to the next connection
+                        if(connection.arrival_time > path_so_far[-1].arrival_time if path_so_far else True):
+                            stack.append((next_city, path_so_far + [connection]))
+            
+        return all_paths
+            
+            
             
     
             
